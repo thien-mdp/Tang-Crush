62973894ad601e74ba2e194c2d39914e7e7d6dff
mdpt999 <thienmai1312@gmail.com>
2021-08-11 06:15:20 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `ㅤㅤㅤㅤNếu tương tư mà không tương tác.
+    ㅤㅤㅤㅤLàm sao tình mình sang trang khác được đây 😔ㅤㅤ
+     Nhấn vào trái tim phía dưới đi nè 🥰🥰`,
+    btnIntro: 'hihi ❤️',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy 🙆 ',
+    btnYes: 'Vẫn cứ là thích anh ❤️',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu =)))',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun ❤️',
+    messLink: 'https://www.facebook.com/thiendz131220/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
